43e77563afc18c492b560facea1928a28c76a15f
richard.slatter <richard.slatter@swyftx.com.au>
2022-04-29 15:37:38 +1000

    added specific HTTP error handlers, implemented within express app

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import {itemsRouter} from "./items/items.router";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
 
 dotenv.config();
 
@@ -30,8 +32,10 @@ const app = express();
 
 app.use(helmet());
 app.use(cors());
-app.use(express.json())
-app.use("/api/menu/items", itemsRouter)
+app.use(express.json());
+app.use("/api/menu/items", itemsRouter);
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 
 /**
